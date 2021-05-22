2f00019ba23b3f3079b7bd69d617de95fe04154c
Zayac11 <brunogrbstudios@gmail.com>
2021-05-22 17:22:48 +0300

    navbar and ui fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import s from './Navbar.module.scss'
 import logo from '../../assets/images/logo.png'
 import gisp from '../../assets/images/gisp_logo.png'
+import {NavLink} from 'react-router-dom'
 
 const Navbar = (props) => {
     return (
         <div className={s.navbar}>
             <div className={s.container}>
-                <img className={s.logo} src={logo} alt='logo' />
-                <img src={gisp} alt='logo_gisp' />
+                <NavLink className={s.images} exact to='/'>
+                    <img className={s.logo} src={logo} alt='logo' />
+                    <img src={gisp} alt='logo_gisp' />
+                </NavLink>
             </div>
         </div>
     )
